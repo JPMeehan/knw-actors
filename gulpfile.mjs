853de1076d3833b1ffa5668c32cc996eea14e0e0
Joseph <JPMeehan5@Gmail.com>
2023-10-17 20:45:18 -0700

    Build Updates

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,34 +1,34 @@
-import gulp from "gulp"
-import dartSass from "node-sass"
-import gulpSass from "gulp-sass"
-import concat from "gulp-concat"
-import { rollup } from "rollup"
+import gulp from "gulp";
+import dartSass from "node-sass";
+import gulpSass from "gulp-sass";
+import concat from "gulp-concat";
+import { rollup } from "rollup";
 
-const sass = gulpSass(dartSass)
+const sass = gulpSass(dartSass);
 
 function compileSCSS() {
   return gulp
     .src("./src/styles/*.scss")
     .pipe(concat("knw-actors.scss"))
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./"))
+    .pipe(gulp.dest("./"));
 }
 
-gulp.task("sass", compileSCSS)
+gulp.task("sass", compileSCSS);
 
 async function compileJavascript() {
   const bundle = await rollup({
     input: "knw-actors.mjs",
     //   plugins: [nodeResolve()]
-  })
+  });
   await bundle.write({
-    file: "public/knw-actors.mjs",
+    file: "./knw-actors-compiled.mjs",
     format: "es",
     sourcemap: true,
     sourcemapFile: "knw-actors.mjs",
-  })
+  });
 }
 
-gulp.task("mjs", compileJavascript)
+gulp.task("mjs", compileJavascript);
 
-gulp.task("buildAll", gulp.series(compileSCSS, compileJavascript))
+gulp.task("buildAll", gulp.series(compileSCSS, compileJavascript));
