4e3b96911d54e5467c7fb7c90625e74fc827b590
Joseph <JPMeehan5@Gmail.com>
2024-01-13 23:55:18 -0800

    Updated Dev Point Configuration Display

--- knw-actors.mjs
+++ knw-actors.mjs
@@ -1,30 +1,35 @@
-import OrganizationData from "./src/module/data/organizationData.mjs"
-import OrganizationSheet from "./src/module/sheets/organizationSheet.mjs"
-import WarfareData from "./src/module/data/warfareData.mjs"
-import WarfareSheet from "./src/module/sheets/warfareSheet.mjs"
-import KNWCONFIG from "./src/module/config.mjs"
+import OrganizationData from './src/module/data/organizationData.mjs';
+import OrganizationSheet from './src/module/sheets/organizationSheet.mjs';
+import WarfareData from './src/module/data/warfareData.mjs';
+import WarfareSheet from './src/module/sheets/warfareSheet.mjs';
+import KNWCONFIG from './src/module/config.mjs';
+import OrgDevEditor from './src/module/sheets/orgDevEditor.mjs';
 
-const moduleID = "knw-actors"
-const typeWarfare = "knw-actors.warfare"
-const typeOrganization = "knw-actors.organization"
+const moduleID = 'knw-actors';
+const typeWarfare = 'knw-actors.warfare';
+const typeOrganization = 'knw-actors.organization';
 
-Hooks.once("init", () => {
-  foundry.utils.mergeObject(CONFIG, KNWCONFIG)
+Hooks.once('init', () => {
+  foundry.utils.mergeObject(CONFIG, KNWCONFIG);
+
+  Handlebars.registerHelper({
+    orgDevRangePicker: OrgDevEditor.rangePicker,
+  });
 
   Object.assign(CONFIG.Actor.dataModels, {
     [typeOrganization]: OrganizationData,
     [typeWarfare]: WarfareData,
-  })
+  });
 
-  Actors.registerSheet("organization", OrganizationSheet, {
+  Actors.registerSheet('organization', OrganizationSheet, {
     types: [typeOrganization],
     makeDefault: true,
-    label: "KNW.Sheets.Organization",
-  })
+    label: 'KNW.Sheets.Organization',
+  });
 
-  Actors.registerSheet("warfare", WarfareSheet, {
+  Actors.registerSheet('warfare', WarfareSheet, {
     types: [typeWarfare],
     makeDefault: true,
-    label: "KNW.Sheets.Warfare",
-  })
-})
+    label: 'KNW.Sheets.Warfare',
+  });
+});
--- src/module/sheets/orgDevEditor.mjs
+++ src/module/sheets/orgDevEditor.mjs
@@ -1,7 +1,33 @@
 export default class OrgDevEditor extends FormApplication {
+  /**
+   * Render a pair of inputs for selecting a value in a range.
+   * @param {object} options                Helper options
+   * @param {string} [options.name]         The name of the field to create
+   * @param {number} [options.value]        The current range value
+   * @param {number} [options.displayValue] The displayed range value
+   * @param {number} [options.min]          The minimum allowed value
+   * @param {number} [options.max]          The maximum allowed value
+   * @param {number} [options.step]         The allowed step size
+   * @returns {Handlebars.SafeString}
+   *
+   * @example
+   * ```hbs
+   * {{rangePicker name="foo" value=bar min=0 max=10 step=1}}
+   * ```
+   */
+  static rangePicker(options) {
+    let { name, value, displayValue, min, max, step } = options.hash;
+    name = name || 'range';
+    value = value ?? '';
+    if (Number.isNaN(value)) value = '';
+    const html = `<input type="range" name="${name}" value="${value}" min="${min}" max="${max}" step="${step}"/>
+     <span class="range-value">${displayValue ?? value}</span>`;
+    return new Handlebars.SafeString(html);
+  }
+
   /** @inheritdoc */
   get template() {
-    return "modules/knw-actors/templates/org-development.hbs";
+    return 'modules/knw-actors/templates/org-development.hbs';
   }
 
   /** @inheritdoc */
@@ -13,19 +39,19 @@ export default class OrgDevEditor extends FormApplication {
   get id() {
     return `${this.constructor.name}-${this.document.uuid.replace(
       /\./g,
-      "-"
+      '-'
     )}-develop-${this.options.statGroup}`;
   }
 
   /** @inheritdoc */
   get title() {
-    return game.i18n.localize("KNW.Organization.Development.Configure");
+    return game.i18n.localize('KNW.Organization.Development.Configure');
   }
 
   /** @inheritdoc */
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ["organization", "orgDev"],
+      classes: ['organization', 'orgDev'],
       closeOnSubmit: false,
       submitOnChange: true,
       width: 360,
@@ -34,31 +60,48 @@ export default class OrgDevEditor extends FormApplication {
 
   /** @inheritdoc */
   async getData(options) {
+    let totalDevPoints = 0;
     const statGroup = this.options.statGroup;
+    /** @type {number[]} */
     const track = CONFIG.KNW.ORGANIZATION.tracks[statGroup];
+    const stats = Object.entries(this.document.system[statGroup]).map(
+      ([key, stat]) => {
+        const headstart = track.findIndex(
+          (v) => v === stat.development.start + stat.development.spec
+        );
+        const displayValue = stat.development.points - headstart;
+        totalDevPoints += displayValue;
+        return {
+          name: `system.${statGroup}.${key}.development`,
+          label:
+            statGroup === 'skills'
+              ? game.i18n.localize('KNW.Organization.skills.' + key)
+              : game.i18n.localize(
+                  'KNW.Organization.defenses.' + key + '.Label'
+                ),
+          displayValue,
+          ...stat,
+        };
+      }
+    );
     const context = {
-      title: game.i18n.localize("KNW.Organization.Development.Configure"),
+      title: game.i18n.localize('KNW.Organization.Development.Configure'),
       ...super.getData(options),
-      stats: Object.entries(this.document.system[statGroup]).map(
-        ([key, stat]) => {
-          return {
-            name: `system.${statGroup}.${key}.development`,
-            label:
-              statGroup === "skills"
-                ? game.i18n.localize("KNW.Organization.skills." + key)
-                : game.i18n.localize(
-                    "KNW.Organization.defenses." + key + ".Label"
-                  ),
-            ...stat,
-          };
-        }
-      ),
+      stats,
+      totalDevPoints,
       track,
-      length: CONFIG.KNW.ORGANIZATION.tracks[statGroup].length - 1,
+      length: track.length - 1,
       min: track[0],
       max: track.slice(-1)[0],
     };
     return context;
+  }
+
+  /** @inheritdoc */
+  render(force = false, options = {}) {
+    // Register the active OrgDevEditor with the referenced Documents
+    this.object.apps[this.appId] = this;
+    return super.render(force, options);
   }
 
   /** @inheritdoc */
